306630211b68b7348572a9645060939dfb396bcc
rupesh672 <=>
2025-12-07 18:25:13 +0545

    feat: authorization server cicd

--- authorization-server-application/src/main/java/com/alfarays/configuration/CorsConfiguration.java
+++ authorization-server-application/src/main/java/com/alfarays/configuration/CorsConfiguration.java
@@ -1,25 +1,23 @@
 package com.alfarays.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class CorsConfiguration {
 
+    @Value("${application.cors.endpoints}")
+    public List<String> CORS_ENDPOINTS;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration corsConfig = new org.springframework.web.cors.CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList(
-                "http://localhost:4200",
-                "http://localhost:3000",
-                "http://localhost:8080",
-                "http://192.168.1.70:8081",
-                "http://192.168.1.70:8181",
-                "http://192.168.1.70:3000"
-        ));
+        corsConfig.setAllowedOrigins(CORS_ENDPOINTS);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
         corsConfig.setAllowCredentials(true);
