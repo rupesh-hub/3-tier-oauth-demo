05b28c51c934431f63e46d7e363b4e4f4d0c7628
rupesh672 <=>
2025-12-08 12:45:22 +0545

    feat: initial commit

--- resource-server-application/src/main/java/com/alfarays/configuration/ResourceServerConfig.java
+++ resource-server-application/src/main/java/com/alfarays/configuration/ResourceServerConfig.java
@@ -3,11 +3,14 @@ package com.alfarays.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,11 +19,26 @@ public class ResourceServerConfig {
     @Value("${application.jwk-set-uri:http://localhost:9000/oauth2/jwks}")
     private String JWK_SET_URI;
 
+    @Value("${application.cors.origins}")
+    private List<String> corsOrigins;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults())
+                .cors(
+                        config -> config.configurationSource(
+                                request -> {
+                                    CorsConfiguration configuration = new CorsConfiguration();
+                                    configuration.setAllowedOrigins(corsOrigins);
+                                    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+                                    configuration.setAllowedHeaders(Arrays.asList("*"));
+                                    configuration.setAllowCredentials(true);
+                                    configuration.setMaxAge(3600L);
+                                    return configuration;
+                                }
+                        )
+                )
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/h2-console/**")
                         .permitAll()
@@ -33,5 +51,4 @@ public class ResourceServerConfig {
 
         return http.build();
     }
-
 }
